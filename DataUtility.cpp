1c6f3f5db48b50455ee1417c8bee24918492f8c7
Krystian Mikolajczak <krystian.mikolajczakk@gmail.com>
2026-06-13 11:45:56 +0200

    data utility Graph generation

--- DataUtility.cpp
+++ DataUtility.cpp
@@ -1,14 +1,15 @@
 #include "DataUtility.hpp"
+#include "Edge.hpp"
+#include "Graph.hpp"
 #include <cstdlib>
-
 void DataUtility::generateGraph(int V, float density, int *matrixDestination,
                                 int **listDestination) {
 
   int *tempMatrix = (int *)std::malloc(V * V * sizeof(int));
-  int **tempList = (int **)std::malloc(V * (sizeof(int *)));
+  Edge **tempList = (Edge **)std::malloc(V * (sizeof(Edge *)));
   int currentEdges = V - 1;
   int randomIndex;
-  int fullGraphEdgeCount = V * V;
+  int fullGraphEdgeCount = (V * V - V) / 2;
   for (int i = 0; i < V * V; i++) {
     tempMatrix[i] = 0;
   }
@@ -21,5 +22,20 @@ void DataUtility::generateGraph(int V, float density, int *matrixDestination,
       tempMatrix[randomIndex] = rand() % 101;
       currentEdges++;
     }
+  }
+  for (int i = 0; i < V; i++) {
+    int EdgeCounter = 0;
+    for (int j = 0; j < V; j++) {
+      if (tempMatrix[i * V + j]) {
+        EdgeCounter++;
+      }
+    }
+    int edgeIndex = 0;
+    Edge *tempNeighbourList = (Edge *)std::malloc(EdgeCounter * sizeof(Edge *));
+    for (int j = 0; j < V; j++) {
+      if (tempMatrix[i * V + j])
+        tempNeighbourList[edgeIndex] = Edge(j, tempMatrix[i * V + j]);
+    }
+    tempList[i] = tempNeighbourList;
   }
 }
--- Edge.hpp
+++ Edge.hpp
@@ -1,8 +1,15 @@
+
+#ifndef EDGE_HPP
+#define EDGE_HPP
 class Edge {
+
   int origin;
   int destination;
   int weight;
 
 public:
+  Edge(int d, int w) : destination(d), weight(w) {}
   Edge(int o, int d, int w) : origin(o), destination(d), weight(w) {}
 };
+
+#endif
